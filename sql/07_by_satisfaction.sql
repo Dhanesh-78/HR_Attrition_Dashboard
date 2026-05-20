431504e9e21398a08e54593747178205e9602963
Dhanesh Nadesan <daneshnadesan@gmail.com>
2026-05-20 11:44:33 +0530

    Create 07_by_satisfaction.sql

---
+++ sql/07_by_satisfaction.sql
@@ -0,0 +1,15 @@
+-- Attrition rate by job satisfaction
+SELECT
+    JobSatisfaction,
+    CASE JobSatisfaction
+        WHEN 1 THEN 'Low'
+        WHEN 2 THEN 'Medium'
+        WHEN 3 THEN 'High'
+        WHEN 4 THEN 'Very High'
+    END                                              AS satisfaction_label,
+    COUNT(*)                                         AS total,
+    SUM(Attrition_num)                               AS left_company,
+    ROUND(SUM(Attrition_num) * 100.0 / COUNT(*), 1) AS attrition_pct
+FROM employees
+GROUP BY JobSatisfaction
+ORDER BY JobSatisfaction;
